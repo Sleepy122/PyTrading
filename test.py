984880f40be8714c33f34dc89cd91a4bff64a03d
Calder Jones <Calder.Jones3155@gmail.com>
2022-02-13 15:55:46 -0600

    revision

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import alpaca_trade_api as tradeapi
-from alpaca_trade_api.rest import REST, TimeFrame
+import secretsConfig as secrets
+
+
 
 ALPACA_BASE_URL = "https://api.alpaca.markets/paper-api.alpaca.markets";
 
 
 class PythonTradingBot :
     def __init__(self) :
-        self.alpaca = tradeapi.REST("PKDA6OSQVESW7FIH53SA","XXCSnmOr3hdOKkCMIe9k1PtWvKxIC4Dp2OpzfnuK", ALPACA_BASE_URL,api_version="v2")
+        self.alpaca = tradeapi.REST(secrets.ALPACA_API_KEY,secrets.ALPACA_API_SECRET, ALPACA_BASE_URL,api_version="v2")
 
+
+
+# test
     def run(self) :
         print("testing getting Microsoft Trade")
         bar_group = self.alpaca.get_barset("QQQ", "day", limit=5)
@@ -17,12 +22,3 @@ class PythonTradingBot :
 
 runner = PythonTradingBot()
 runner.run()
-
-# api = REST("6471159df356db467b5301ca1598859f","484202e887c2d3d382cf676830bdb406309b6a0a", ALPACA_BASE_URL,api_version="v2")
-# def process_bar(bar):
-#     # process bar
-#     print(bar)
-
-# bar_iter = api.alpaca.ge("AAPL", TimeFrame.Hour, "2021-06-08", "2021-06-08", adjustment='raw')
-# for bar in bar_iter:
-#     process_bar(bar)
